a2b1b8efd276a30ad99c32782f590e9c579c28c0
GitHub Action <action@github.com>
2022-07-22 10:34:40 +0000

    Template cleanup

---
+++ src/main/kotlin/com/github/gepengcn/myideaplugintemplate/services/MyApplicationService.kt
@@ -0,0 +1,10 @@
+package com.github.gepengcn.myideaplugintemplate.services
+
+import com.github.gepengcn.myideaplugintemplate.MyBundle
+
+class MyApplicationService {
+
+    init {
+        println(MyBundle.message("applicationService"))
+    }
+}
